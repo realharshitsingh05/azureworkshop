3e0a3a3291a3b77a1c603876b43813c2a9fba6d9
realharshitsingh05 <104898355+realharshitsingh05@users.noreply.github.com>
2022-05-05 16:05:04 +0530

    Updated my name

--- portfolio-template/index.js
+++ portfolio-template/index.js
@@ -1,6 +1,6 @@
-const name = 'Your Name'
-const title = 'Your Title'
-const mail = 'Your Email'
+const name = 'Harshit Singh'
+const title = 'Creator'
+const mail = 'harshisingh0508@gmail.com'
 
 document.getElementById('title').innerText = name
 document.getElementById('name').innerText = name
